37efea0ec2c061571f3040f938321df199ddd585
Caio Colaiacovo <caiohshot@gmail.com>
2018-05-22 22:40:03 -0400

    Creating context

--- dotnet-core-elasticsearch/dotnet-core-elasticsearch.Data/Context/EFContext.cs
+++ dotnet-core-elasticsearch/dotnet-core-elasticsearch.Data/Context/EFContext.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using dotnet_core_elasticsearch.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace dotnetcoreelasticsearch.Data.Context
 {
     public class EFContext : DbContext
     {
-        
+        //public EFContext(DbContextOptions<EFContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer("Data source=localhost,1433;Initial Catalog=TestDB; User ID=usertest; Password=user@test123");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
     }
 }
