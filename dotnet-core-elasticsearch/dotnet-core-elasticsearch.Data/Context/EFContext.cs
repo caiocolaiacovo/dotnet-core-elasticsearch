e051169f22851fbfec41052d3532a69ece4450f0
Caio Colaiacovo <caiohshot@gmail.com>
2018-06-01 23:07:29 -0400

    Fixing context namespace

--- dotnet-core-elasticsearch/dotnet-core-elasticsearch.Data/Context/EFContext.cs
+++ dotnet-core-elasticsearch/dotnet-core-elasticsearch.Data/Context/EFContext.cs
@@ -2,12 +2,10 @@
 using dotnet_core_elasticsearch.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace dotnetcoreelasticsearch.Data.Context
+namespace dotnet_core_elasticsearch.Data.Context
 {
     public class EFContext : DbContext
     {
-        //public EFContext(DbContextOptions<EFContext> options) : base(options) { }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
         {
             optionsBuilder.UseSqlServer("Data source=localhost,1433;Initial Catalog=TestDB; User ID=usertest; Password=user@test123");
